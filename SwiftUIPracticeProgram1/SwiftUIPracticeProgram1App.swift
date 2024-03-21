47a943ec42b5249273d5d7e5b3f8aa4a95324cf0
anjalis-df <anjalis@dream-filler.com>
2024-03-21 17:29:07 +0530

    Initial Commit

---
+++ SwiftUIPracticeProgram1/SwiftUIPracticeProgram1App.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIPracticeProgram1App.swift
+//  SwiftUIPracticeProgram1
+//
+//  Created by Anjali Sikarwar on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeProgram1App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
